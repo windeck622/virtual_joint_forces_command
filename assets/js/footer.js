807fbbece98d65726941a908a092b5f5370f08af
cynamons <windeck.s48@gmail.com>
2025-01-10 16:20:27 -0600

    Update Mission Statement

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -2,7 +2,7 @@ var yearValue = new Date().getFullYear();
 
 let appFooter = `
     <footer class="footer">
-        <p class="infos">&copy; ${yearValue} | Virtual Joint Forces Command | Design by <a href="http://www.devcrud.com">DevCRUD</a></p>       
+        <p class="infos">&copy; ${yearValue} | Virtual Joint Forces Command Inc.       
         <span>|</span>  
         <div class="links">
             <a href="index.html#About-Us-Section">About Us</a>
@@ -10,6 +10,8 @@ let appFooter = `
             <a href="train.html#">Training</a>
             <a href="rules.html#">Rules</a>
         </div>
+        <span>|</span> 
+        <div>Design by <a href="http://www.devcrud.com">DevCRUD</a></p></div>
     </footer><!-- end of footer -->
 `;
 document.getElementById("app-footer").innerHTML = appFooter;
